66ddc569e6ac891ca5a109d738bb91e3f6d8af35
abanoub-adel <abanoub25adel@gmail.com>
2022-10-28 19:33:51 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+#question a
+"""num1=int(input("Enter your first number:"))
+num2=int(input("Enter your second number: "))
+if(num1>num2):
+    print("first number is greatest")
+elif(num2>num1):
+    print("second number is greatest")"""
+
+
+
